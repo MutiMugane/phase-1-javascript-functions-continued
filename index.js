e288fa49e202e2cd8156aaf5b248cf4b82a51c68
+ <+>
2022-08-23 14:29:01 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(activity="go to the office"){
+    return (`This Monday, I will ${activity}.`);
+}
+
+function wrapAdjective(flame='*'){{
+        {
+            return function(description='special'){
+                return `You are ${flame}${description}${flame}!`;
+            }
+        }
+      // const encouragingPromptFunction = wrapAdjective("!!!")
+};
+    
+}
